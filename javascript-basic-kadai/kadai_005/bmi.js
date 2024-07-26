f9586a5f28b04401231f272b15838bbc26df7195
sosugaya <sosugaya@gmail.com>
2024-07-27 04:08:54 +0900

    update BMIの計算結果&BMIの計算結果も変数に代入

--- javascript-basic-kadai/kadai_005/bmi.js
+++ javascript-basic-kadai/kadai_005/bmi.js
@@ -1,15 +1,19 @@
  // 変数の宣言
  let userWeight;
  let userHeight;
+ let BMI;
 
  // 値の代入
  userWeight = 68;
  userHeight = 1.7;
+ BMI = (userWeight/(userHeight*userHeight));
+
 
  // コンソールへの出力
  console.log('体重:'+(userWeight)+'kg');
  console.log('身長:'+(userHeight)+'m');
 
  // 四則演算
- console.log('BMI:'+ (userWeight/(userHeight*userWeight)).toFixed(2)+'kg/m²');
+ console.log('BMI:' + BMI +'kg/m2');
+ 
 
