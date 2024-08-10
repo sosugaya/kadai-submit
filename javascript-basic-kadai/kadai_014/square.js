7ba1369ab636e6beb4a6f0f9861df0dc9c991df1
sosugaya <sosugaya@gmail.com>
2024-08-11 05:21:32 +0900

    update square.js 引数で渡した値を二乗した計算結果を戻り値として返す関数を記述

--- javascript-basic-kadai/kadai_014/square.js
+++ javascript-basic-kadai/kadai_014/square.js
@@ -1,10 +1,12 @@
-//1以上の数をランダムに宣言
-const num = Math.floor(Math.random() * 10) + 1;
 
-// 関数の中で数字を２乗
-const squareTotal =()=>{
-console.log(num ** 2)
+//引数で渡した値を二乗した計算結果を戻り値として返す関数を記述
+
+// 与えられた引数numを2乗にし、その値を戻り値として返す関数を定義する
+
+const squareTotal =(num)=>{
+  return num ** 2;
 }
 
-// 関数を呼び出す
-squareTotal ()
+// 関数の戻り値を出力する
+console.log(squareTotal(Math.floor(Math.random() * 10) + 1));
+
