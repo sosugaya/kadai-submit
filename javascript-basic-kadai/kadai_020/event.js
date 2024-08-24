649fe416d4d149a2a1d5041c79512b00bbc4d989
sosugaya <sosugaya@gmail.com>
2024-08-25 05:41:47 +0900

    Update2 event.js follow assignment instruction

--- javascript-basic-kadai/kadai_020/event.js
+++ javascript-basic-kadai/kadai_020/event.js
@@ -1,16 +1,18 @@
 //画面から取得したHTML要素が定数に代入されている
 //ボタンをクリックした際に正しく文字変更がされている
 
-//ボタンをクリックする
-//
 
 //btnというidを持つHTML要素を取得し、定数に代入する
 const clickBtn = document.getElementById('btn');
 
+// textというidを持つh2要素を取得し、定数に代入する
+const textElement = document.getElementById('text');
+
 // HTML要素がクリックされたときにイベント処理を実行する
 clickBtn.addEventListener('click', () => {
-  console.log('クリックされました！');
-});
-//h2要素を作成する
+  
+  //作成したh2要素に「ボタンをクリックしました」というテキストを追加する
+ textElement.textContent='ボタンをクリックしました';
 
-//作成したh2要素に「ボタンをクリックしました」というテキストを追加する
+});
+
