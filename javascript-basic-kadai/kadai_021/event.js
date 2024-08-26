e26d35d945cb4d7b67715df2cd2483e1c36d8365
sosugaya <sosugaya@gmail.com>
2024-08-27 06:35:23 +0900

    update event.js, which followed the kadai021 instruction

--- javascript-basic-kadai/kadai_021/event.js
+++ javascript-basic-kadai/kadai_021/event.js
@@ -4,10 +4,18 @@ const clickBtn = document.getElementById ('btn');
 // textというidを持つh2要素を取得し、定数に代入する
 const textElement = document.getElementById ('text');
 
-// 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
-setTimeout (() => {
-  clickBtn.addEventListener('click',() => {
+//コードの可読性向上
+const updateText = () => {
+  textElement.textContent='ボタンをクリックしました';
+}
 
-     //作成したh2要素に「ボタンをクリックしました」というテキストを追加する
- textElement.textContent='ボタンをクリックしました';});
- },2000);
+
+// HTML要素がクリックされたときにイベント処理を実行する
+clickBtn.addEventListener('click',() => {
+
+  // 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+  setTimeout ( 
+// updateText関数として定義して、それを setTimeout に渡す場合
+ updateText,2000);
+
+});
